ccab05fe29ef92a017db8cc8b6b08b408692b548
Victor Gonzalez <mywalletislost@gmail.com>
2018-09-20 19:19:07 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,6 @@ sentence.split.each {|string| string.reverse.join(" ")}
 
 end
  def reverse_each_word(sentence)
-  backward = sentence.split.collect{|string| string.reverse.join(" ")}
+sentence.split.collect{|string| string.reverse.join(" ")}
 
 end 
